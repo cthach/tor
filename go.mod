6ed101cbc8aaf56c491d4cf75107ecd1cceca5f0
Chris Thach <12981621+cthach@users.noreply.github.com>
2020-06-24 07:24:31 -0700

    Add func stubs for exit node check

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cthach/tornet
+
+go 1.14
---
+++ node.go
@@ -0,0 +1,24 @@
+package tornet
+
+import (
+	"context"
+	"net"
+)
+
+// IsExit returns nil if the IP address belongs to an exit node.
+func IsExit(ctx context.Context, ip net.IP) error {
+	// TODO
+	return nil
+}
+
+// queryDNSExitList queries the Tor Project's DNS based node exit list and returns the response, if any.
+func queryDNSExitList(ctx context.Context, src net.IP, dst net.IP, port uint16) ([]net.IPAddr, error) {
+	// TODO
+	return nil, nil
+}
+
+// reverseIP returns an IP with octets in reverse order.
+func reverseIP(ip net.IP) net.IP {
+	// TODO
+	return nil
+}
